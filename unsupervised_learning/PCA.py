c5c5dd59b318669f80910a7fcca979dc89a5727a
YanlunZhu <100049729+YanlunZhu@users.noreply.github.com>
2022-04-12 11:17:10 +0100

    Update PCA.py

--- unsupervised_learning/PCA.py
+++ unsupervised_learning/PCA.py
@@ -1,3 +1,13 @@
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Machine leanring for financial analysis by Yanying Guan, Yanlun Zhu is licensed under 
+# a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+# Based on a work at https://realized.oxford-man.ox.ac.uk/.
+# Copy this code to let your visitors know!
+# <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Machine leanring for financial analysis</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="https://github.com/YanyingGuan; https://github.com/YanlunZhu" property="cc:attributionName" rel="cc:attributionURL">YanyingGuan, YanlunZhu</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.<br />Based on a work at <a xmlns:dct="http://purl.org/dc/terms/" href="https://realized.oxford-man.ox.ac.uk/" rel="dct:source">https://realized.oxford-man.ox.ac.uk/</a>.
+# Copyright (c) March 2022 Yanying Guan, Yanlun Zhu. All rights reserved.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
 import pandas as pd
 import numpy as np
 import seaborn as sns
